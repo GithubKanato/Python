4b420adeef08995ef29dd071dac922dd9bec427f
GithubKanatom <detectiveboys710@gmail.com>
2024-05-26 14:29:18 +0900

    連絡先管理アプリ

--- day2/contact_managemen_app.py
+++ day2/contact_managemen_app.py
@@ -9,16 +9,51 @@ def display_menu():
 def add_contact(contact):
     name = input("氏名:")
     tel = input("電話番号:")
+    contact[name] = tel
+    return contact
 
+def disp_contact(contact):
+    if contact:
+        for name, tel in contact.items():
+            print(f"氏名：{name} , 電話番号：{tel}")
+    else:
+        print("連絡先は登録されていません")
+    return contact
+
+def search_contact(contact):
+    search_word = input("誰の電話番号を知りたいですか？")
+    if search_word in contact:
+        print(f"{search_word}の電話番号は{contact[search_word]}です")
+    else:
+        print("登録されていません")
+
+def delete_contact(contact):
+    delete_name = input("誰の情報を消去しますか？")
+    if delete_name in contact:
+        contact.pop(delete_name)
+        print(f"{delete_name}の連絡先を削除しました")
+    else:
+        print("登録されていません")
+    return contact
 
 def main():
     contact = {}
-    display_menu()
-    val = input("何をしますか？")
-    while val != 5:
-        if val == 1:
-            add_contact(contact)
-    
+    loop_cnt = True
+    while loop_cnt:
+        display_menu()
+        val = input("何をしますか？")
+        if val == "1":
+            contact = add_contact(contact)
+        elif val == "2":
+            contact = disp_contact(contact)
+        elif val == "3":
+            search_contact(contact)
+        elif val == "4":
+            contact = delete_contact(contact)
+        elif val == "5":
+            loop_cnt = False
+        else:
+            print("無効な選択です。もう一度お試しください。")
 
 if __name__ == "__main__":
     main()
